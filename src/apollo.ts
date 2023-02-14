04ed48b51fa2951860bf02045b53fa4a747e1db9
cafe small house <tbaram10@naver.com>
2023-02-14 23:50:13 +0900

    #11.9 Apollo Client 완료

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,4 +1,49 @@
-import { makeVar } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink, makeVar, split } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { LOCAL_STORAGE_TOKEN } from "./constants";
 
-export const isLoggedInVar = makeVar(false);
-export const darkModeVar = makeVar(false);
+const token = localStorage.getItem(LOCAL_STORAGE_TOKEN);
+
+export const isLoggedInVar = makeVar(Boolean(token));
+export const authTokenVar = makeVar(token);
+
+const httpLink = createHttpLink({
+   uri: "http://localhost:5000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+   return {
+      headers: {
+         ...headers,
+         "x-jwt": authTokenVar() || "",
+      },
+   };
+});
+
+const splitLink = split(({ query }) => {
+   const definition = getMainDefinition(query);
+   return definition.kind === "OperationDefinition" && definition.operation === "subscription";
+}, authLink.concat(httpLink));
+
+export const client = new ApolloClient({
+   link: splitLink,
+   cache: new InMemoryCache({
+      typePolicies: {
+         Query: {
+            fields: {
+               isLoggedIn: {
+                  read() {
+                     return isLoggedInVar();
+                  },
+               },
+               token: {
+                  read() {
+                     return authTokenVar();
+                  },
+               },
+            },
+         },
+      },
+   }),
+});
