8369504382da39fa69aa5b2567e187d254e0ab82
cafe small house <tbaram10@naver.com>
2023-03-22 19:44:12 +0900

    #12.18 Create Comment part Three 완료

--- src/components/feed/comments.component.tsx
+++ src/components/feed/comments.component.tsx
@@ -77,11 +77,26 @@ const Comments = ({ photoId, author, caption, commentNumber, comments }: ICommen
                ...userData.me,
             },
          };
+         const newCacheComment = cache.writeFragment({
+            data: newComment,
+            fragment: gql`
+               fragment CommentFragment on Comment {
+                  id
+                  createdAt
+                  isMine
+                  payload
+                  user {
+                     username
+                     avatar
+                  }
+               }
+            `,
+         });
          cache.modify({
             id: `Photo:${photoId}`,
             fields: {
                comments(prev) {
-                  return [...prev, newComment];
+                  return [...prev, newCacheComment];
                },
                commentNumber(prev) {
                   return prev + 1;
